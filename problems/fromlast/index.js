fe53e405b7a8d0e56f774e45cae7ae48807302f0
Steven Inouye <steven.inouye91@gmail.com>
2023-02-19 13:06:31 -0800

    checking what linked list node is n from last nade

---
+++ problems/fromlast/index.js
@@ -0,0 +1,36 @@
+
+// --- Directions
+// Given a linked list, return the element n spaces
+// from the last node in the list.  Do not call the 'size'
+// method of the linked list.  Assume that n will always
+// be less than the length of the list.
+// --- Examples
+//    const list = new List();
+//    list.insertLast('a');
+//    list.insertLast('b');
+//    list.insertLast('c');
+//    list.insertLast('d');
+//    fromLast(list, 2).data // 'b'
+
+const linked = require('./linkedlist')
+const Node = linked.Node;
+const List = linked.LinkedList;
+
+function fromLast(list, n) {
+    let slow=list.getAt(0);
+    let fast=list.getAt(0+n);
+    while(fast.next){
+        slow = slow.next;
+        fast = fast.next
+    }
+    return slow
+}
+
+   const list = new List();
+   list.insertLast('a');
+   list.insertLast('b');
+   list.insertLast('c');
+   list.insertLast('d');
+   console.log(fromLast(list, 3)) // 'b'
+
+module.exports = fromLast;
